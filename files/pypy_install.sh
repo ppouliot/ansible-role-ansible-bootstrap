51d853621fff507ef98c22f9d45732d4cf105fbe
Peter Pouliot <peter@pouliot.net>
2020-09-16 22:52:23 +0000

    trying to make pypy_install.sh multiplatform

--- files/pypy_install.sh
+++ files/pypy_install.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 PYTHON_VERSION="3.6"
 PYPY_VERSION="7.3.1"
 UNAME=`which uname`
-
-echo "* UNAME="${UNAME}
+CURL=`which curl`
+WGET=`which wget`
 ARCH=`${UNAME} -m`
-echo "* ARCH="${ARCH}
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   OS="linux"
@@ -33,11 +32,16 @@ else
   exit
 fi
 
-PYPY_FILE=pypy${PYTHON_VERSION}-v${PYPY_VERSION}-${PYPY_ARCH}.tar.bz2
-PYPY_DIR=pypy${PYTHON_VERSION}-v${PYPY_VERSION}-${PYPY_ARCH}
-PYPY_URL=https://downloads.python.org/pypy/${PYPY_FILE}
+PYPY_FILE=pypy${PYTHON_VERSION}-v${PYPY_VERSION}-${PYPY_ARCH}
+PYPY_URL=https://downloads.python.org/pypy/${PYPY_FILE}.tar.bz2
 
 
+
+echo "**** Variables Used ****"
+echo "* UNAME="${UNAME}
+echo "* CURL="${CURL}
+echo "* WGET="${WGET}
+echo "* ARCH="${ARCH}
 echo "* OS="${OS}
 echo "* PYTHON_VERSION="${PYTHON_VERSION}
 echo "* PYPY_VERSION="${PYPY_VERSION}
@@ -73,18 +77,26 @@ echo "Downloading PYPY Portable binary: $PYPY_PORTABLE_VERSION"
 # wget -nv https://bitbucket.org/squeaky/portable-pypy/downloads/$PYPY_PORTABLE_VERSION.tar.bz2 ;
 # https://downloads.python.org/pypy/pypy3.6-v7.3.1-linux64.tar.bz2
 
-wget -nv ${PYPY_URL} ;
+if [ -f "${WGET}" ];then
+  ${WGET} -nv ${PYPY_URL} ;
+elif [ -f "${CURL}" ];then
+  ${CURL} ${PYPY_URL} --output ${PYPY_FILE}.tar.bz
+else
+  echo "Download clients curl or wget not found!"
+  exit
+fi
+
 cd $HOME/.ansible
 
 echo "Extracting PYPY Portable binary: $PYPY_PORTABLE_VERSION"
 cd $HOME/.ansible
-tar -xjf tmp/$PYPY_FILE
+tar -xjf tmp/${PYPY_FILE}.tar.bz
 
 echo "Renaming ${PYPY_DIR} to pypy"
-mv ${PYPY_DIR} pypy
+mv ${PYPY_FILE} pypy
 
 echo "Removing downloaded PYPY tarball file ${PYPY_FILE}"
-rm -rf tmp/$PYPY_FILE
+rm -rf tmp/${PYPY_FILE}.tar.bz
 
 echo "Setting resursive permissions on pypy directory."
 chmod -R ugo+x $HOME/.ansible/pypy
@@ -100,5 +112,6 @@ if [ ! -e $HOME/.bash_profile ];
 then
   cat <<EOF > $HOME/.bash_profile
 export PATH=${HOME}/bin:$PATH
+export LDFLAGS="-L${HOME}/.ansible/pypy/bin"
 EOF
 fi
